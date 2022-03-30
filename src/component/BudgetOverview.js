83121b7e9d83386088d47599edd87e8a2205ab72
Heidi <heidi.willden@me.com>
2022-03-29 19:07:22 -0500

    Styling and layout for budget overview

--- src/component/BudgetOverview.js
+++ src/component/BudgetOverview.js
@@ -1,11 +1,30 @@
 import React from "react";
+import "./BudgetOverview.css";
 
 function BudgetOverview(props) {
   return (
     <div className="BudgetOverview">
-      <h3>Budget: $xxx</h3>
-      <h3>Remaining: $xxx</h3>
-      <h3>Spent so far: $xxx</h3>
+      <h1>My Budget Planner</h1>
+      <div className="row overview-row">
+        <div className="col overview-item budget">
+          <div className="row">
+            <div className="col">
+              <h4>Budget: $xxx</h4>
+            </div>
+            <div className="col">
+              <button className="btn btn-primary edit-budget">Edit</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col overview-item remainder">
+          <h4>Remaining: $xxx</h4>
+        </div>
+
+        <div className="col overview-item total-expenditures">
+          <h4>Spent so far: $xxx</h4>
+        </div>
+      </div>
     </div>
   );
 }
