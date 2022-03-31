aa4972b0418b5a9788f8318794d18cb7b67bfc24
Heidi <heidi.willden@me.com>
2022-03-30 19:33:46 -0500

    Save function
    Set budget value to input value

--- src/component/BudgetOverview.js
+++ src/component/BudgetOverview.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BudgetOverview.css";
 
 function BudgetOverview(props) {
+  const [budget, setBudget] = useState(0);
+
   function editBudget() {
     // Hide current budget
     const budgetDisplay = document.getElementById("budgetDisplay");
@@ -19,13 +21,36 @@ function BudgetOverview(props) {
     const saveBtn = document.getElementById("saveBtn");
     saveBtn.classList.remove("hidden");
   }
+
+  function saveBudget(event) {
+    event.preventDefault();
+    setBudget(event.target.value);
+
+    // Change budget value to input value
+    const budgetInput = document.getElementById("budgetInput");
+    setBudget(budgetInput.value);
+    budgetInput.classList.add("hidden");
+
+    // Show budget display with new budget value
+    const budgetDisplay = document.getElementById("budgetDisplay");
+    budgetDisplay.classList.remove("hidden");
+
+    // Show edit button
+    const editBtn = document.getElementById("editBtn");
+    editBtn.classList.remove("hidden");
+
+    // Show save button
+    const saveBtn = document.getElementById("saveBtn");
+    saveBtn.classList.add("hidden");
+  }
+
   return (
     <div className="BudgetOverview">
       <h1>My Budget Planner</h1>
       <div className="row overview-row">
         <div className="col overview-item budget">
           <h4 id="budgetDisplay" className="">
-            Budget: $xxx
+            Budget: ${budget}
           </h4>
           <input type="number" id="budgetInput" className="hidden"></input>
           <button
@@ -37,7 +62,7 @@ function BudgetOverview(props) {
           </button>
           <button
             className="btn btn-primary save-budget-btn hidden"
-            onClick={editBudget}
+            onClick={saveBudget}
             id="saveBtn"
           >
             Save
