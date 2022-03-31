b10070ab8d464779de5776c239d1134eef787950
Heidi <heidi.willden@me.com>
2022-03-31 18:51:00 -0500

    Add expenses to list
    Append expenses and costs to list. Bootstrap styling not maintained on new elements

--- src/component/ExpensesList.js
+++ src/component/ExpensesList.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpensesList.css";
 
 function ExpensesList(props) {
+  const items = [];
+  const costs = [];
+
+  function createListLine(name, cost) {
+    // Get title into string
+    let listTemplate = `
+       <li className="list-group-item">
+            <div className="list-item">
+              <span id="itemName">${name}</span>
+              <div>
+                <span className="badge bg-primary" id="itemCost">${cost}</span>
+                <button className="delete-btn">
+                  <i className="bi bi-x-circle-fill"></i>
+                </button>
+              </div>
+            </div>
+          </li>`;
+    return listTemplate;
+  }
+
+  // Add new  expense input to list when + button is clicked
+  function addExpense(event) {
+    event.preventDefault();
+
+    // Set expense (item) name to name  input
+    let itemName = document.getElementById("itemName");
+    let itemInput = document.getElementById("itemInput").value;
+    itemName = itemInput;
+    // Push item to array of items
+    items.push(itemName);
+
+    // Set item cost to cost input
+    let itemCost = document.getElementById("itemCost");
+    let costInput = document.getElementById("costInput").value;
+    itemCost = costInput;
+    // Push cost to array of costs
+    items.push(itemCost);
+
+    //  Append new expense to list
+    let todoListLocal = document.querySelector("#expensesList");
+    let newTodoItem = createListLine(itemName, itemCost);
+
+    // Turn template HTML into DOM node
+    let wrapper = document.createElement("ul");
+    wrapper.innerHTML = newTodoItem;
+
+    // Append new node to todo list
+    let newListLineAsElement = wrapper.querySelector("li");
+    todoListLocal.append(newListLineAsElement);
+  }
+
   return (
     <div>
       <h2>Expenses</h2>
 
       <div className="card">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
+        <ul className="list-group list-group-flush" id="expensesList">
+          {/* <li className="list-group-item">
             <div className="list-item">
               <span>First item</span>
               <div>
@@ -29,14 +80,26 @@ function ExpensesList(props) {
                 </button>
               </div>
             </div>
-          </li>
+          </li> */}
         </ul>
       </div>
 
       <div className="input-group add-input">
-        <input type="text" className="form-control" placeholder="Item" />
-        <input type="text" className="form-control" placeholder="Cost" />
-        <button className="btn btn-primary add-btn">+</button>
+        <input
+          type="text"
+          className="form-control"
+          id="itemInput"
+          placeholder="Item"
+        />
+        <input
+          type="number"
+          className="form-control"
+          id="costInput"
+          placeholder="Cost"
+        />
+        <button className="btn btn-primary add-btn" onClick={addExpense}>
+          +
+        </button>
       </div>
     </div>
   );
