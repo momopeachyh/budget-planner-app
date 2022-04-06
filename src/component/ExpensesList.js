340e706e0324daab62766cde0c2500caba91cdc9
Heidi <heidi.willden@me.com>
2022-04-05 20:51:29 -0500

    Updated filter function

--- src/component/ExpensesList.js
+++ src/component/ExpensesList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./ExpensesList.css";
 
@@ -6,6 +6,7 @@ function ExpensesList() {
   const dispatch = useDispatch();
   const expensesList = useSelector((state) => state.expenses);
   const expenseCount = useSelector((state) => state.expenseCount);
+  const [searchTerm, setSearchTerm] = useState("");
 
   function addExpense(e) {
     e.preventDefault();
@@ -38,57 +39,51 @@ function ExpensesList() {
     });
   }
 
-  // function expenseExists () {
-  //   if (expense.name === searchInput) {
-  //     console.log("true")
-  //   } else {
-  //     console.log("false")
-  //   }
-  // }
-
-  function searchExpenses(e) {
-    e.preventDefault();
-    const searchInput = document.getElementById("searchInput").value;
-    const expenseExists = (expense) => expense.name == searchInput;
-    if (expensesList.some(expenseExists)) {
-      console.log("true");
-    } else {
-      console.log("false");
-    }
-  }
-
   return (
     <div>
       <h2>Expenses</h2>
       <div className="row search-bar">
         <div className="col-10">
-          <input type="text" className="form-control" id="searchInput" />
+          <input
+            type="text"
+            className="form-control"
+            id="searchInput"
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
         </div>
         <div className="col-2">
-          <button className="btn btn-primary" onClick={searchExpenses}>
-            Search
-          </button>
+          <button className="btn btn-primary">Search</button>
         </div>
       </div>
       <div className="card">
         <ul className="list-group list-group-flush" id="expensesList">
-          {expensesList.map((expense) => (
-            <li className="list-group-item" key={expense.key}>
-              <div className="list-item">
-                <span id="listItem">{expense.name}</span>
-                <div>
-                  <span className="badge bg-primary">${expense.cost}</span>
-                  <button
-                    className="delete-btn"
-                    id="deleteBtn"
-                    onClick={() => deleteExpense(expense.key, expense.cost)}
-                  >
-                    <i className="bi bi-x-circle-fill"></i>
-                  </button>
+          {expensesList
+            .filter((expense) => {
+              if (searchTerm == "") {
+                return expense;
+              } else if (expense.name.includes(searchTerm.toLowerCase())) {
+                return expense;
+              }
+            })
+            .map((expense) => (
+              <li className="list-group-item" key={expense.key}>
+                <div className="list-item">
+                  <span id="listItem">{expense.name}</span>
+                  <div>
+                    <span className="badge bg-primary">${expense.cost}</span>
+                    <button
+                      className="delete-btn"
+                      id="deleteBtn"
+                      onClick={() => deleteExpense(expense.key, expense.cost)}
+                    >
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))}
         </ul>
       </div>
 
